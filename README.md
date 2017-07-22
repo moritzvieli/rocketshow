fd179557f77669f1490846f8f88a123d4acc56c3
Moritz Vieli <m.vieli@ametiqconnect.com>
2017-07-22 13:32:21 +0200

    Changed the name, fixed some minor problems

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ShowMachine
 A tool to automate your DMX and videoshow.
+
+Access the web api with the following url: http://localhost:8080/ShowMachine/api/status/
--- src/com/ascargon/showmachine/midi/Startup.java
+++ src/com/ascargon/showmachine/midi/Startup.java
@@ -116,58 +116,58 @@ public class Startup {
             }
             //System.exit(1);
         }
-        MidiDevice inputDevice = null;
-        try {
-            inputDevice = MidiSystem.getMidiDevice(info);
-            inputDevice.open();
-        } catch (MidiUnavailableException e) {
-            out(e);
-        }
-        if (inputDevice == null) {
-            out("wasn't able to retrieve MidiDevice");
-            System.exit(1);
-        }
-        Receiver r = new DumpReceiver(System.out);
-        try {
-            Transmitter t = inputDevice.getTransmitter();
-            t.setReceiver(r);
-        } catch (MidiUnavailableException e) {
-            out("wasn't able to connect the device's Transmitter to the Receiver:");
-            out(e);
-            inputDevice.close();
-            System.exit(1);
-        }
-        if (bUseDefaultSynthesizer) {
-            Synthesizer synth = MidiSystem.getSynthesizer();
-            synth.open();
-            r = synth.getReceiver();
-            try {
-                Transmitter t = inputDevice.getTransmitter();
-                t.setReceiver(r);
-            } catch (MidiUnavailableException e) {
-                out("wasn't able to connect the device's Transmitter to the default Synthesizer:");
-                out(e);
-                inputDevice.close();
-                System.exit(1);
-            }
-        }
-        out("now running; interupt the program with [ENTER] when finished");
-
-        try {
-            System.in.read();
-        } catch (IOException ioe) {
-        }
-        inputDevice.close();
-        out("Received " + ((DumpReceiver) r).seCount + " sysex messages with a total of " + ((DumpReceiver) r).seByteCount + " bytes");
-        out("Received " + ((DumpReceiver) r).smCount + " short messages with a total of " + ((DumpReceiver) r).smByteCount + " bytes");
-        out("Received a total of " + (((DumpReceiver) r).smByteCount + ((DumpReceiver) r).seByteCount) + " bytes");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            if (DEBUG) {
-                out(e);
-            }
-        }
+//        MidiDevice inputDevice = null;
+//        try {
+//            inputDevice = MidiSystem.getMidiDevice(info);
+//            inputDevice.open();
+//        } catch (MidiUnavailableException e) {
+//            out(e);
+//        }
+//        if (inputDevice == null) {
+//            out("wasn't able to retrieve MidiDevice");
+//            System.exit(1);
+//        }
+//        Receiver r = new DumpReceiver(System.out);
+//        try {
+//            Transmitter t = inputDevice.getTransmitter();
+//            t.setReceiver(r);
+//        } catch (MidiUnavailableException e) {
+//            out("wasn't able to connect the device's Transmitter to the Receiver:");
+//            out(e);
+//            inputDevice.close();
+//            System.exit(1);
+//        }
+//        if (bUseDefaultSynthesizer) {
+//            Synthesizer synth = MidiSystem.getSynthesizer();
+//            synth.open();
+//            r = synth.getReceiver();
+//            try {
+//                Transmitter t = inputDevice.getTransmitter();
+//                t.setReceiver(r);
+//            } catch (MidiUnavailableException e) {
+//                out("wasn't able to connect the device's Transmitter to the default Synthesizer:");
+//                out(e);
+//                inputDevice.close();
+//                System.exit(1);
+//            }
+//        }
+//        out("now running; interupt the program with [ENTER] when finished");
+//
+//        try {
+//            System.in.read();
+//        } catch (IOException ioe) {
+//        }
+//        inputDevice.close();
+//        out("Received " + ((DumpReceiver) r).seCount + " sysex messages with a total of " + ((DumpReceiver) r).seByteCount + " bytes");
+//        out("Received " + ((DumpReceiver) r).smCount + " short messages with a total of " + ((DumpReceiver) r).smByteCount + " bytes");
+//        out("Received a total of " + (((DumpReceiver) r).smByteCount + ((DumpReceiver) r).seByteCount) + " bytes");
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            if (DEBUG) {
+//                out(e);
+//            }
+//        }
     }
 
 
