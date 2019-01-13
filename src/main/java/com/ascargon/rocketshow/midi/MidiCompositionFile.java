dc636345d3b36bb4bfeb96da3dba4d16a69be1de
Moritz Vieli <m.vieli@ametiqconnect.com>
2019-01-13 13:12:39 +0100

    Fixed MIDI file upload

--- src/main/java/com/ascargon/rocketshow/midi/MidiCompositionFile.java
+++ src/main/java/com/ascargon/rocketshow/midi/MidiCompositionFile.java
@@ -9,7 +9,6 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import com.ascargon.rocketshow.composition.CompositionFile;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-@JsonTypeName("midi")
 public class MidiCompositionFile extends CompositionFile {
 
     private List<MidiRouting> midiRoutingList = new ArrayList<>();
