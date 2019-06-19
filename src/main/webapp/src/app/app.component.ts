75b2a9f2acdeaa084efe60f29dc6c77001b4af21
Moritz Vieli <m.vieli@ametiqconnect.com>
2019-06-19 09:14:04 +0200

    Show a loading spinner while loading a slow page

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { LeadSheetService } from './services/lead-sheet.service';
 import { StateService } from './services/state.service';
 import { CompositionService } from './services/composition.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart, NavigationCancel } from '@angular/router';
 import { forkJoin } from 'rxjs';
 import { SessionService } from './services/session.service';
 import { SettingsService } from './services/settings.service';
@@ -17,11 +17,12 @@ import { OperatingSystemInformationService } from './services/operating-system-i
   styleUrls: ['./app.component.scss'],
   host: { '[class.body-bg-moving]': 'this.isIntro' }
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   isIntro: boolean = false;
   isPlay: boolean = false;
   loaded: boolean = false;
+  loadingPage: boolean = false;
   settings: Settings;
 
   constructor(
@@ -35,6 +36,7 @@ export class AppComponent implements OnInit {
     private leadSheetService: LeadSheetService,
     private operatingSystemInformationService: OperatingSystemInformationService) {
 
+    this.loadingPage = true;
     translateService.setDefaultLang('en');
   }
 
@@ -52,20 +54,20 @@ export class AppComponent implements OnInit {
         this.isIntro = false;
         this.isPlay = false;
 
-        switch(e.url) { 
-          case '/intro': { 
-             this.isIntro = true;
-             break; 
-          } 
-          case '/play': { 
-             this.isPlay = true;
-             break; 
+        switch (e.url) {
+          case '/intro': {
+            this.isIntro = true;
+            break;
           }
-          case '/': { 
+          case '/play': {
             this.isPlay = true;
-            break; 
-         }
-       } 
+            break;
+          }
+          case '/': {
+            this.isPlay = true;
+            break;
+          }
+        }
       }
     });
 
@@ -97,12 +99,27 @@ export class AppComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationStart) {
+          this.loadingPage = true;
+        }
+        else if (
+          event instanceof NavigationEnd ||
+          event instanceof NavigationCancel
+        ) {
+          this.loadingPage= false;
+        }
+      });
+  }
+
   showLeadSheet() {
     this.leadSheetService.show();
   }
 
-  leadSheetButtonVisible(): boolean  {
-    if(this.leadSheetService.currentLeadSheetUrl) {
+  leadSheetButtonVisible(): boolean {
+    if (this.leadSheetService.currentLeadSheetUrl) {
       return true;
     }
 
